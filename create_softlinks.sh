d14b420d891a3100934ae67c18305930ac5676dd
Randy West <randywest55@gmail.com>
2019-10-10 18:26:49 -0400

    - Added .gitconfig + .zshrc - Moved old stuff to an archive dir - Added create_softlinks.sh

---
+++ create_softlinks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# Create softlinks to all of the dot files in script directory
+PATH_TO_DOT_FILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+rm $HOME/.zshrc
+ln -s $PATH_TO_DOT_FILES/.zshrc $HOME/.zshrc
+rm $HOME/.jsbeautifyrc
+ln -s $PATH_TO_DOT_FILES/.jsbeautifyrc $HOME/.jsbeautifyrc
+rm $HOME/.gitconfig
+ln -s $PATH_TO_DOT_FILES/.gitconfig $HOME/.gitconfig
+rm $HOME/.config/nvim/init.vim
+ln -s $PATH_TO_DOT_FILES/init.vim $HOME/.config/nvim/init.vim
+
