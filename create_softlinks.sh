be81235b0c9cf67dd954f46530c9b6d5d8691c0c
Randy West <randywest55@gmail.com>
2019-10-13 20:31:47 -0400

    Create links from nvim.appimage to vim/nvim

--- create_softlinks.sh
+++ create_softlinks.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Create softlinks to all of the dot files in script directory
 PATH_TO_DOT_FILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm $HOME/.zshrc
@@ -11,4 +11,7 @@ rm $HOME/.config/nvim/init.vim
 ln -s $PATH_TO_DOT_FILES/init.vim $HOME/.config/nvim/init.vim
 rm $HOME/.config/nvim/coc-settings.json
 ln -s $PATH_TO_DOT_FILES/coc-settings.json $HOME/.config/nvim/coc-settings.json
-
+rm $HOME/bin/vim
+ln -s $HOME/bin/nvim.appimage $HOME/bin/vim
+rm $HOME/bin/nvim
+ln -s $HOME/bin/nvim.appimage $HOME/bin/nvim
