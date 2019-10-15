ef21f92ff7c4eac293e2bc8c788ef0f596831f63
Randy West <randywest55@gmail.com>
2019-10-15 18:00:22 -0400

    Add vscode-install-extensions.sh

---
+++ vscode-install-extensions.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+# Programmatically install VSCode extensions that I might otherwise
+# forget about (not automatically recommended)
+
+code --install-extension hoovercj.vscode-settings-cycler
+
