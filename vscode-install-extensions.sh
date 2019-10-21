00a9b76327af8afd94a81dd56fdb6fe120cbcffb
Randy West <randywest55@gmail.com>
2019-10-20 20:07:28 -0400

    Add numbered-bookmarks

--- vscode-install-extensions.sh
+++ vscode-install-extensions.sh
@@ -9,3 +9,4 @@ code --install-extension vscode-icons-team.vscode-icons
 code --install-extension zignd.html-css-class-completion
 code --install-extension eamodio.gitlens
 code --install-extension github.vscode-pull-request-github
+code --install-extension alefragnani.numbered-bookmarks
