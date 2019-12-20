e8f670ec0b162b439aef23d8b46bb87166c650e3
Randy West <randywest55@gmail.com>
2019-12-20 18:25:20 -0500

    Add gnome-stuff.sh

---
+++ gnome-stuff.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# USAGE: ./gnome-stuff.sh
+# Note that there are a few manual steps that I couldn't automate:
+# - Configure weather location (logout to enable in top bar)
+# - Open tweaks and set theme to Adwaita-dark
+# - Install Clock Override extension and set format string to %A %B %e %l:%M:%S
+
+sudo apt-get install gnome-weather gnome-tweaks
