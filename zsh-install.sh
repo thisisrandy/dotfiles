9c4643011ac3f27c080dd678e62d9e0aef432580
Randy West <randywest55@gmail.com>
2022-07-19 13:04:58 -0700

    Update USAGE/notes

--- zsh-install.sh
+++ zsh-install.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env zsh
-# USAGE: ./zsh-install.sh
-# NOTES:
-#   - Certain setup commands need to be run in zsh and not bash. This script
-#     exists to run those commands
-#   - This script is intended to be run after install.sh is complete. In fact,
-#     the last thing install.sh does is invoke this script. As such, it should
-#     not be run twice, though doing so may not harm anything
+# USAGE: ZSH_CUSTOM=$ZSH_CUSTOM ./zsh-install.sh
+#
+# It's unclear how to run scripts in the context of Oh-My-Zsh, which creates a
+# bunch of variables w/o exporting them to the environment. It's also unclear
+# if the main installer script has any opportunity to invoke things in the OMZ
+# context. In lieu of a better option, this script can be invoked manually from
+# OMZ as specified in the USAGE
 
 # poetry tab completion. see
 # https://python-poetry.org/docs/#enable-tab-completion-for-bash-fish-or-zsh
