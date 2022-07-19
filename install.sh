11cfeafc6f97c90dd8aaeccea5dd6ee89143c768
Randy West <randywest55@gmail.com>
2022-07-19 03:56:45 -0700

    [Fix] Add nodejs installation back in

--- install.sh
+++ install.sh
@@ -67,6 +67,7 @@ popd
 
 # install node/yarn
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 npm config set prefix ~/.local # prevents need to sudo for -g
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
