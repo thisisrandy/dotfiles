be39bf0250ba314bd6cc0c0cc773e0addee416d4
Randy West <randywest55@gmail.com>
2022-07-19 04:02:54 -0700

    Remove manual instructions where automation found

--- gnome-stuff.sh
+++ gnome-stuff.sh
@@ -5,14 +5,9 @@
 # - Open tweaks and set theme to Adwaita-dark
 # - Install Clock Override extension and set format string to %A %B %e %l:%M:%S
 # - Connect google account for calendar (online accounts)
-# - Turn off terminal bell in term preferences, and set font to Menlo for
-#   Powerline, size 11
 # - Change alt/super-tab behavior. Settings->Devices->Keyboard->Switch windows/applications
 # - If running with multiple monitors, set tweaks->Workspaces->Workspaces Span Displays
 # - To enhance window tiling, follow https://askubuntu.com/a/1089033/1014459
-# - Turn repeat keys delay down to at least tmux repeat-time. See
-#   https://help.ubuntu.com/stable/ubuntu-help/keyboard-repeat-keys.html.en for
-#   details
 
 sudo apt-get install gnome-weather gnome-tweaks
 # if the system is dual boot with windows, run this (from
