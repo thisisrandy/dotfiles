3ea806e6e0b37ab0e8b7c114414158788aa48ea0
Randy West <randywest55@gmail.com>
2022-10-24 17:23:31 -0700

    Add gh installation

--- install.sh
+++ install.sh
@@ -320,3 +320,10 @@ wget https://dl.flathub.org/repo/appstream/fr.handbrake.ghb.flatpakref
 sudo flatpak install -y fr.handbrake.ghb.flatpakref
 popd
 
+# install gh (github cli)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | \
+   sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
