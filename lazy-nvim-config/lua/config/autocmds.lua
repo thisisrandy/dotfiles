609013e15f9570e62143930518b4f00128985959
Randy West <randywest55@gmail.com>
2024-09-30 13:06:21 -0700

    Add autocmds from my noplug init file

--- lazy-nvim-config/lua/config/autocmds.lua
+++ lazy-nvim-config/lua/config/autocmds.lua
@@ -1,3 +1,29 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+local cursor_line_only_in_active_window = augroup("CursorLineOnlyInActiveWindow")
+vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+  group = cursor_line_only_in_active_window,
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+vim.api.nvim_create_autocmd("WinLeave", {
+  group = cursor_line_only_in_active_window,
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
+
+local term_insert = augroup("TermInsert")
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = term_insert,
+  callback = function()
+    vim.cmd.startinsert()
+  end,
+})
