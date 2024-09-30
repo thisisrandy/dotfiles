add5994dcc4cba3376ca5b7929e1552a4d98922d
Randy West <randywest55@gmail.com>
2024-09-30 08:11:35 -0700

    Add lazygit installation

--- install.sh
+++ install.sh
@@ -419,3 +419,11 @@ sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.loca
 echo 'kitty.desktop' > ~/.config/xdg-terminals.list
 # Create a symbolic link to the config file
 ln -sf $PATH_TO_DOT_FILES/kitty.conf $HOME/.config/kitty/
+
+# install lazygit
+pushd $(mktemp -d)
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+popd
