7994ef1fcbf43f83fdad0a2de9ae06f51b972891
Randy West <randywest55@gmail.com>
2024-10-01 05:42:30 -0700

    Comment unused scrollback config

--- lazy-nvim-config/lua/plugins/kitty.lua
+++ lazy-nvim-config/lua/plugins/kitty.lua
@@ -1,16 +1,19 @@
 return {
-  {
-    "mikesmithgh/kitty-scrollback.nvim",
-    enabled = true,
-    lazy = true,
-    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-    event = { "User KittyScrollbackLaunch" },
-    -- version = '*', -- latest stable version, may have breaking changes if major version changed
-    -- version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
-    config = function()
-      require("kitty-scrollback").setup()
-    end,
-  },
+  -- I'm currently usring kitty_grab instead of this because of the awkwardness
+  -- of https://github.com/mikesmithgh/kitty-scrollback.nvim/issues/273. Should
+  -- that be resolved, I may switch back
+  -- {
+  --   "mikesmithgh/kitty-scrollback.nvim",
+  --   enabled = true,
+  --   lazy = true,
+  --   cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+  --   event = { "User KittyScrollbackLaunch" },
+  --   -- version = '*', -- latest stable version, may have breaking changes if major version changed
+  --   -- version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
+  --   config = function()
+  --     require("kitty-scrollback").setup()
+  --   end,
+  -- },
 
   {
     "knubie/vim-kitty-navigator",
