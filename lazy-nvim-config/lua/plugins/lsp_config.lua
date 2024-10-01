61f22158fdf184003e5e12521de4c2edf1f5c028
Randy West <randywest55@gmail.com>
2024-10-01 03:36:10 -0700

    Swap out currated python config for basic

--- lazy-nvim-config/lua/plugins/lsp_config.lua
+++ lazy-nvim-config/lua/plugins/lsp_config.lua
@@ -1,14 +1,4 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        pyright = {},
-      },
-    },
-  },
-
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
 }
