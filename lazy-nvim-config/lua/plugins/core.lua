fbeb2d369c9758c6bc1f6001e4fa0c2f18ba44f6
Randy West <randywest55@gmail.com>
2024-10-02 15:49:29 -0700

    Move fzf (install-only) into core config file

--- lazy-nvim-config/lua/plugins/core.lua
+++ lazy-nvim-config/lua/plugins/core.lua
@@ -1,3 +1,11 @@
 -- Put plugins that need no further configuration here. Otherwise, create a
 -- per-plugin or per-plugin group file and store configuration there
-return { { "fladson/vim-kitty" } }
+return {
+  {
+    "junegunn/fzf",
+    build = function()
+      vim.fn["fzf#install"]()
+    end,
+  },
+  { "fladson/vim-kitty" },
+}
--- lazy-nvim-config/lua/plugins/fzf.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "junegunn/fzf",
-    build = function()
-      vim.fn["fzf#install"]()
-    end,
-  },
-}
