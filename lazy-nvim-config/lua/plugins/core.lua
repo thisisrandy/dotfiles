5346414df6e8ec169c9ffc1cc19aedecef27caa2
Randy West <randywest55@gmail.com>
2024-10-02 15:14:40 -0700

    Add vim-kitty plugin
    Provide kitty conf plugin highlighting

--- lazy-nvim-config/lua/plugins/core.lua
+++ lazy-nvim-config/lua/plugins/core.lua
@@ -1,1 +1,3 @@
-return { {} }
+-- Put plugins that need no further configuration here. Otherwise, create a
+-- per-plugin or per-plugin group file and store configuration there
+return { { "fladson/vim-kitty" } }
