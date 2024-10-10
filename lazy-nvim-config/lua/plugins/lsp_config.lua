ad31bbcbf2534b7add641267b538b24aa05620f4
Randy West <randywest55@gmail.com>
2024-10-09 21:10:24 -0700

    Add some additional language plugins

--- lazy-nvim-config/lua/plugins/lsp_config.lua
+++ lazy-nvim-config/lua/plugins/lsp_config.lua
@@ -6,6 +6,12 @@ return {
       { mode = "n", "<F2>", "<leader>cr", desc = "Rename", remap = true },
     },
   },
+  { import = "lazyvim.plugins.extras.lang.haskell" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 }
