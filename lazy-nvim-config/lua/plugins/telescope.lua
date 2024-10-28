667b01c7146898dad37c30de49b23378b1fa9bcf
Randy West <randywest55@gmail.com>
2024-10-28 10:40:48 -0700

    Remove custom under word grep mappings
    It turns out LazyVim (or some plugin it uses) has these built in, and
    they do a better job than my bindings because they appear to sanitize
    regex characters first

--- lazy-nvim-config/lua/plugins/telescope.lua
+++ lazy-nvim-config/lua/plugins/telescope.lua
@@ -13,37 +13,6 @@ return {
       -- faster and more familiar file/buffer switching
       { mode = "n", ";", "<leader>,", desc = "Switch buffers", remap = true },
       { mode = "n", "<C-p>", "<leader>ff", desc = "Find files (Root Dir),", remap = true },
-      -- Under cursor grep shortcuts.
-      -- Note that the following don't account for special regex chars in any way.
-      -- Copied text has the potential to be a malformed expression
-      {
-        mode = "n",
-        "<leader>su",
-        '"zyiw<leader>sg<c-r>z',
-        desc = "Grep for word [u]nder cursor (Root Dir),",
-        remap = true,
-      },
-      {
-        mode = "n",
-        "<leader>sU",
-        '"zyiw<leader>sG<c-r>z',
-        desc = "Grep for word [U]nder cursor (cwd),",
-        remap = true,
-      },
-      {
-        mode = "v",
-        "<leader>su",
-        '"zy<leader>sg<c-r>z',
-        desc = "Grep for highlighted word ([u]nder cursor), (Root Dir),",
-        remap = true,
-      },
-      {
-        mode = "v",
-        "<leader>sU",
-        '"zy<leader>sG<c-r>z',
-        desc = "Grep for highlighted word ([U]nder cursor), (cwd),",
-        remap = true,
-      },
     },
     opts = {
       defaults = {
