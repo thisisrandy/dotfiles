b9fe5d60b2eaa8e14cc88af34b23e0d5a7e97d4f
Randy West <randywest55@gmail.com>
2024-11-19 04:26:21 -0800

    Add custom surround T to preserve tag attributes

--- lazy-nvim-config/lua/plugins/surround.lua
+++ lazy-nvim-config/lua/plugins/surround.lua
@@ -2,5 +2,19 @@ return {
   "echasnovski/mini.surround",
   opts = {
     n_lines = 100,
+    custom_surroundings = {
+      -- Preserves tag attributes. See
+      -- https://github.com/echasnovski/mini.nvim/issues/1293#issuecomment-2423827325
+      T = {
+        input = { "<(%w+)[^<>]->.-</%1>", "^<()%w+().*</()%w+()>$" },
+        output = function()
+          local tag_name = MiniSurround.user_input("Tag name")
+          if tag_name == nil then
+            return nil
+          end
+          return { left = tag_name, right = tag_name }
+        end,
+      },
+    },
   },
 }
