4c2a83536c151a163c5cb8a56d67d72de882bbf9
Randy West <randywest55@gmail.com>
2024-11-19 04:33:19 -0800

    Add guibg highlight for MatchParen

--- lazy-nvim-config/lua/plugins/core.lua
+++ lazy-nvim-config/lua/plugins/core.lua
@@ -11,5 +11,13 @@ return {
   { "andymass/vim-matchup" },
   { -- NOTE: This is on gitlab, not github
     "HiPhish/rainbow-delimiters.nvim",
+    config = function(_, opts)
+      require("rainbow-delimiters.setup").setup(opts)
+      -- With rainbow delimiters on, a fg color change often isn't easily
+      -- distinguishable, so we need a stronger visual cue. This is the same
+      -- background color used in tokyonight to highlight to object under the
+      -- cursor and other instances of it.
+      vim.cmd([[highlight MatchParen guibg=#3b4261]])
+    end,
   },
 }
