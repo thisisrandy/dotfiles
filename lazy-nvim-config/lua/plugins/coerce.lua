ac52757c8eaa42812b21739ed3ef219580f053cc
Randy West <randywest55@gmail.com>
2024-11-19 05:34:02 -0800

    Add coerce.nvim

---
+++ lazy-nvim-config/lua/plugins/coerce.lua
@@ -0,0 +1,10 @@
+return {
+  "gregorias/coerce.nvim",
+  opts = {
+    default_mode_keymap_prefixes = {
+      normal_mode = "<leader>r",
+      motion_mode = "<leader>gr",
+      visual_mode = "<leader>gr",
+    },
+  },
+}
