6c5e17a752b64fddc7b3fe40aeee5e33bfa71cda
Randy West <randywest55@gmail.com>
2024-11-18 19:04:52 -0800

    Tweak mini.files config

--- lazy-nvim-config/lua/plugins/mini.files.lua
+++ lazy-nvim-config/lua/plugins/mini.files.lua
@@ -23,4 +23,16 @@ return {
       desc = "Open mini.files (root)",
     },
   },
+  opts = {
+    windows = {
+      width_preview = 50,
+    },
+    options = {
+      -- I honestly don't understand what this does, but
+      -- https://www.lazyvim.org/extras/editor/mini-files#minifiles says that
+      -- it's disabled by default because neo-tree is used by default, so I
+      -- guess that means I should have it on...?
+      use_as_default_explorer = true,
+    },
+  },
 }
