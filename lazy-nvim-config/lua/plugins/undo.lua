5e25981a8578f751b6149b61cce3a79c6467a65c
Randy West <randywest55@gmail.com>
2024-11-18 23:37:16 -0800

    Make undotree load lazily

--- lazy-nvim-config/lua/plugins/undo.lua
+++ lazy-nvim-config/lua/plugins/undo.lua
@@ -2,6 +2,7 @@ return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
   config = true,
+  lazy = true,
   keys = { -- load the plugin only when using it's keybinding:
     { "<leader>n", "<cmd>lua require('undotree').toggle()<cr>", desc = "Open u[n]dotree" },
   },
