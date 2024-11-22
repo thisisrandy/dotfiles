afdbb4ca1d56f99fc34fb29304d568d13019d6e1
Randy West <randywest55@gmail.com>
2024-11-21 20:37:46 -0800

    Unify coerce mappings + add labels

--- lazy-nvim-config/lua/plugins/coerce.lua
+++ lazy-nvim-config/lua/plugins/coerce.lua
@@ -1,10 +1,18 @@
 return {
   "gregorias/coerce.nvim",
-  opts = {
-    default_mode_keymap_prefixes = {
-      normal_mode = "<leader>r",
-      motion_mode = "<leader>gr",
-      visual_mode = "<leader>gr",
-    },
-  },
+  config = function(_, opts)
+    require("coerce").setup(vim.tbl_deep_extend("force", opts, {
+      default_mode_keymap_prefixes = {
+        normal_mode = "<leader>rw",
+        motion_mode = "<leader>rm",
+        visual_mode = "<leader>rs",
+      },
+    }))
+    require("which-key").add({
+      { mode = { "n", "v" }, "<leader>r", group = "Coe[r]ce" },
+      { "<leader>rw", group = "Word" },
+      { "<leader>rm", group = "Motion" },
+      { mode = "v", "<leader>rs", group = "Selection" },
+    })
+  end,
 }
