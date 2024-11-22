e0d6d99c1dad0db5c5ef180778c7034bf26d3431
Randy West <randywest55@gmail.com>
2024-11-22 15:41:33 -0800

    Make note about pending PR

--- lazy-nvim-config/lua/plugins/lualine.lua
+++ lazy-nvim-config/lua/plugins/lualine.lua
@@ -7,6 +7,8 @@ return {
           { "progress", separator = " ", padding = { left = 1, right = 0 } },
           -- This is a substitute for the default "location" that includes the
           -- total line number
+          -- TODO: If #1334 is accepted, I can enable this with
+          -- line_total_in_location = true in opts
           {
             function()
               local line = vim.fn.line(".")
