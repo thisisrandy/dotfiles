d83bfd3da9e7b5afbbea4f8d89d28f28840f985f
Randy West <randywest55@gmail.com>
2024-11-25 21:24:07 -0800

    Add N American date format + note about format ambiguity

--- lazy-nvim-config/lua/plugins/dial.lua
+++ lazy-nvim-config/lua/plugins/dial.lua
@@ -5,10 +5,13 @@ return {
       local augend = require("dial.augend")
 
       -- Add a few date/time formats to defaults
-      -- FIXME: Dates like "%m/%d/%Y" don't work properly. My initial guess is
-      -- that if the format isn't most to least significant then there are
-      -- problems. I should file a bug at some point
+      -- NOTE: We can introduce a lot of ambiguity with dial formats. The
+      -- following three work as expected, but we can't also have e.g.
+      -- %m-%d-%Y, because, since we have negative decimals enabled, dial
+      -- decides that %d and %Y are standalone negative decimals and not part
+      -- of a date
       vim.list_extend(opts.groups.default, {
+        augend.date.alias["%m/%d/%Y"],
         augend.date.alias["%Y-%m-%d"],
         augend.date.alias["%H:%M:%S"],
       })
