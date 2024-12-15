1e5c51223d1a9c45e8718057fba6dc808cdc34b2
Randy West <randywest55@gmail.com>
2024-12-15 11:00:21 -0800

    Update deprecated use defaults flag

--- lazy-nvim-config/lua/plugins/motions.lua
+++ lazy-nvim-config/lua/plugins/motions.lua
@@ -2,7 +2,7 @@ return {
   {
     "chrisgrieser/nvim-various-textobjs",
     event = "UIEnter",
-    opts = { useDefaultKeymaps = true },
+    opts = { keymaps = { useDefaults = true } },
   },
   {
     "chrisgrieser/nvim-spider",
