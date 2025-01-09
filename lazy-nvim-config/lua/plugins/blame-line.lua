70d5cfae9d8ecf1cfa77691c3a9957f00b94903e
Randy West <randywest55@gmail.com>
2025-01-08 21:00:39 -0800

    Use delay-improvements branch

--- lazy-nvim-config/lua/plugins/blame-line.lua
+++ lazy-nvim-config/lua/plugins/blame-line.lua
@@ -1,5 +1,8 @@
 return {
-  "braxtons12/blame_line.nvim",
+  -- TODO: Change this back to braxtons12/master after
+  -- https://github.com/braxtons12/blame_line.nvim/pull/13 is accepted
+  "thisisrandy/blame_line.nvim",
+  branch = "delay-improvements",
   opts = {
     prefix = "        ",
     delay = 500,
