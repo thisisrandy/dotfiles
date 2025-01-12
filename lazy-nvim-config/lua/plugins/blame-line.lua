81de8b3504b2fcc8e4f1b63bc5351d575f2be98a
Randy West <randywest55@gmail.com>
2025-01-11 19:30:58 -0800

    Change blame branch to unmerged-changes

--- lazy-nvim-config/lua/plugins/blame-line.lua
+++ lazy-nvim-config/lua/plugins/blame-line.lua
@@ -2,7 +2,7 @@ return {
   -- TODO: Change this back to braxtons12/master after
   -- https://github.com/braxtons12/blame_line.nvim/pull/13 is accepted
   "thisisrandy/blame_line.nvim",
-  branch = "delay-improvements",
+  branch = "unmerged-changes",
   opts = {
     prefix = "   ",
     delay = 500,
