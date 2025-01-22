70c2c94fb70b31de26062d35b3cfd0477e9353b4
Randy West <randywest55@gmail.com>
2025-01-22 08:21:13 -0800

    Swap char order for tab listchars

--- lazy-nvim-config/lua/config/options.lua
+++ lazy-nvim-config/lua/config/options.lua
@@ -6,4 +6,4 @@ vim.opt.spell = true
 vim.opt.dictionary = "/usr/share/dict/words"
 vim.opt.virtualedit = "onemore"
 vim.opt.list = false
-vim.opt.listchars = "space:·,multispace:····+,tab:￫-,eol:¶,trail:~,extends:>,precedes:<,nbsp:⍽"
+vim.opt.listchars = "space:·,multispace:····+,tab:-￫,eol:¶,trail:~,extends:>,precedes:<,nbsp:⍽"
