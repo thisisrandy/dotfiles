bba2658e4f57f2d240ce3a47b868bebf21ada264
Randy West <randywest55@gmail.com>
2026-04-06 18:13:40 -0700

    Update delta version

--- install.sh
+++ install.sh
@@ -375,8 +375,8 @@ sudo perl -pi -e 's/(EXTRA_OPTS=).*/$1"--timeout 2"/' /etc/default/unclutter
 
 # install delta
 pushd $(mktemp -d)
-wget https://github.com/dandavison/delta/releases/download/0.16.5/git-delta_0.16.5_amd64.deb
-sudo dpkg -i git-delta_0.16.5_amd64.deb
+wget https://github.com/dandavison/delta/releases/download/0.19.2/git-delta_0.19.2_amd64.deb
+sudo dpkg -i git-delta_0.19.2_amd64.deb
 popd
 
 # install navi
