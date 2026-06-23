27b94f0ada095e3824f623ce9e6d1b9d005dd375
Randy West <randywest55@gmail.com>
2026-06-22 19:32:33 -0700

    Add gnome-browser-connector to install list

--- install.sh
+++ install.sh
@@ -39,7 +39,8 @@ sudo apt-get -y install curl git zsh xclip htop iftop gcc make \
     jq tmux peek datamash nmap bvi httpie ripgrep \
     gnome-weather gnome-tweaks tree mkvtoolnix perl-doc fortunes \
     cowsay at linux-tools-common linux-tools-generic ranger sshfs \
-    figlet whois default-jre moreutils xsel dict lshw gnome-boxes
+    figlet whois default-jre moreutils xsel dict lshw gnome-boxes \
+    gnome-browser-connector
 
 ln -sf $PATH_TO_DOT_FILES/.gitconfig $HOME/.gitconfig
 mkdir -p $HOME/.config/ranger
