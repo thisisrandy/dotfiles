e9c5c9f8dcaa59f3e6f31129da433cf83bad461f
Randy West <randywest55@gmail.com>
2026-06-26 14:56:38 -0700

    Remove nvim_rocks dep

--- lazy-nvim-config/lua/plugins/motions.lua
+++ lazy-nvim-config/lua/plugins/motions.lua
@@ -13,12 +13,17 @@ return {
       { mode = { "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-b" },
       { mode = { "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-ge" },
     },
-    dependencies = {
-      "theHamsta/nvim_rocks",
-      build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
-      config = function()
-        require("nvim_rocks").ensure_installed("luautf8")
-      end,
-    },
+    -- It's unclear why I have this here. I found a mention of it as a
+    -- workaround to some utf8 issues at
+    -- https://github.com/chrisgrieser/nvim-spider/issues/14#issuecomment-1631931792,
+    -- but it's currently failing to run its config and hanging vim when spider is loaded
+    -- for the first time, so I'm going to try doing without it
+    -- dependencies = {
+    --   "theHamsta/nvim_rocks",
+    --   build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+    --   config = function()
+    --     require("nvim_rocks").ensure_installed("luautf8")
+    --   end,
+    -- },
   },
 }
