d76cc433dcdfcee076f14083ac336abbf929f930
Randy West <randywest55@gmail.com>
2026-06-25 19:19:34 -0700

    Add spider ge mostion and normalize formatting

--- lazy-nvim-config/lua/plugins/motions.lua
+++ lazy-nvim-config/lua/plugins/motions.lua
@@ -10,12 +10,8 @@ return {
     keys = {
       { mode = { "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
       { mode = { "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-e" },
-      {
-        mode = { "n", "o", "x" },
-        "b",
-        "<cmd>lua require('spider').motion('b')<CR>",
-        desc = "Spider-b",
-      },
+      { mode = { "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-b" },
+      { mode = { "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-ge" },
     },
     dependencies = {
       "theHamsta/nvim_rocks",
