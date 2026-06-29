cafb4a419f9bf76f72427d4b52bd09d4ae996b82
Randy West <randywest55@gmail.com>
2026-06-29 15:24:59 -0700

    Add and install lazygit configuration

--- install.sh
+++ install.sh
@@ -483,6 +483,8 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+mkdir -p ~/.config/lazygit/ # Probably not necessary
+ln -sf ~/git/dotfiles/lazygit-config.yml ~/.config/lazygit/config.yml
 popd
 
 # install fd
