9da538b7d0b9e610f330641cffbe70a343aaaeb1
Randy West <randywest55@gmail.com>
2026-06-30 20:59:41 -0700

    Use built-in preset for super-tab and disable <CR> handling

--- lazy-nvim-config/lua/plugins/blink.lua
+++ lazy-nvim-config/lua/plugins/blink.lua
@@ -1,33 +1,13 @@
 return {
   "saghen/blink.cmp",
-  -- From https://cmp.saghen.dev/configuration/keymap#super-tab
   opts = {
     keymap = {
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-
-      ["<Tab>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        "snippet_forward",
-        "fallback",
-      },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      -- See https://cmp.saghen.dev/configuration/keymap#super-tab
+      preset = "super-tab",
+      -- super-tab doesn't disable blink's default Enter functionality. This
+      -- is super annoying when we actually want a new line, so we disable it
+      -- explicitly
+      ["<CR>"] = {},
     },
   },
 }
