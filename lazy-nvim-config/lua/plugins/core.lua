083725089f7d8ad849bb03ce0386bcec86762a9e
Randy West <randywest55@gmail.com>
2026-07-01 15:20:46 -0700

    Turn sync_with_ring back on + note

--- lazy-nvim-config/lua/plugins/core.lua
+++ lazy-nvim-config/lua/plugins/core.lua
@@ -22,10 +22,13 @@ return {
     "gbprod/yanky.nvim",
     config = {
       system_clipboard = {
-        -- This plants some VimEnter/VimLeave autocommands that cause gnome to
-        -- rapidly cycle focus of the next selected application, which causes
-        -- some ugly flickering
-        sync_with_ring = false,
+        -- This uses getreg (see get_register_info in utils.lua), which
+        -- apparently briefly loses focus when it queries the system clipboard,
+        -- which it turn causes gnome to flicker. Based on the defer logic in
+        -- system_clipboard.setup(), this is a known issue, so it seems
+        -- unlikely to be fixed. In fact, even just "+p causes a flicker. I
+        -- think I'm going to just deal with it
+        sync_with_ring = true,
       },
     },
   },
