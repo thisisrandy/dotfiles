e8542349c0a353d1daf56e5375b39fdec51440b5
Randy West <randywest55@gmail.com>
2026-07-02 14:37:34 -0700

    Switch back to main origin

--- lazy-nvim-config/lua/plugins/blame-line.lua
+++ lazy-nvim-config/lua/plugins/blame-line.lua
@@ -1,8 +1,5 @@
 return {
-  -- TODO: Change this back to braxtons12/master after
-  -- https://github.com/braxtons12/blame_line.nvim/pull/13 is accepted
-  "thisisrandy/blame_line.nvim",
-  branch = "unmerged-changes",
+  "braxtons12/blame_line.nvim",
   opts = {
     prefix = "   ",
     delay = 500,
