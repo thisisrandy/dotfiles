d76b7dec53f57ecc93be31f65d8502fa1f96c369
Randy West <randywest55@gmail.com>
2026-07-02 14:39:28 -0700

    Disable blame_line (conflict w/hover text)

--- lazy-nvim-config/lua/plugins/blame-line.lua
+++ lazy-nvim-config/lua/plugins/blame-line.lua
@@ -1,5 +1,8 @@
 return {
   "braxtons12/blame_line.nvim",
+  -- FIXME: This plugin causes hover text to flicker at the top of the screen
+  -- before disappearing. I may attempt to hunt down the issue at some point
+  enabled = false,
   opts = {
     prefix = "   ",
     delay = 500,
