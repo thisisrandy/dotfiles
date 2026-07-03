59af74852ae246be00142b1b6ebf1f39cb562ed0
Randy West <randywest55@gmail.com>
2026-07-02 17:42:41 -0700

    Remove redundant configuration
    When using "super-tab" as a preset (instead of pasting in the config the
    way I had it previously) <CR> isn't mapped to accept. I think not using
    a preset loaded up some defaults before applying my config, hence the
    need to explicitly disable it.

--- lazy-nvim-config/lua/plugins/blink.lua
+++ lazy-nvim-config/lua/plugins/blink.lua
@@ -4,10 +4,6 @@ return {
     keymap = {
       -- See https://cmp.saghen.dev/configuration/keymap#super-tab
       preset = "super-tab",
-      -- super-tab doesn't disable blink's default Enter functionality. This
-      -- is super annoying when we actually want a new line, so we disable it
-      -- explicitly
-      ["<CR>"] = {},
     },
   },
 }
