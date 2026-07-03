72f6c66315e4a3e436fb657b86bbefe265799234
Randy West <randywest55@gmail.com>
2026-07-02 22:01:57 -0700

    Remove empty line

--- lazy-nvim-config/lua/plugins/core.lua
+++ lazy-nvim-config/lua/plugins/core.lua
@@ -1,7 +1,6 @@
 -- Put plugins that need no further configuration here. Otherwise, create a
 -- per-plugin or per-plugin group file and store configuration there
 return {
-
   { "fladson/vim-kitty" },
   { "andymass/vim-matchup" },
 }
