1291b0b7723d1159e51a51598c2c317460f3983a
Randy West <randywest55@gmail.com>
2026-07-08 19:31:08 -0700

    Handle javascript preview formatting

--- ranger-scope.sh
+++ ranger-scope.sh
@@ -303,7 +303,7 @@ handle_mime() {
         ;;
 
     ## Text
-    text/* | */xml)
+    text/* | */xml | */javascript)
         ## Syntax highlight
         if [[ "$(stat --printf='%s' -- "${FILE_PATH}")" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
             exit 2
