f4254face6b80c4c7a76590916cb827ac3dfea24
Randy West <randywest55@gmail.com>
2026-07-09 17:36:25 -0700

    Explicitly capture window number

--- lazy-nvim-config/lua/plugins/snacks.nvim.lua
+++ lazy-nvim-config/lua/plugins/snacks.nvim.lua
@@ -28,14 +28,14 @@ return {
 
                 -- Instead, we'll open it in a scratch split
                 local buf = vim.api.nvim_create_buf(false, true)
-                vim.api.nvim_open_win(buf, true, {
+                local win = vim.api.nvim_open_win(buf, true, {
                   split = "below",
                   height = 15,
                 })
                 vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(item.text, "\n"))
-                vim.api.nvim_win_set_buf(0, buf)
+                vim.api.nvim_win_set_buf(win, buf)
                 vim.keymap.set("n", "q", function()
-                  vim.api.nvim_win_close(0, false)
+                  vim.api.nvim_win_close(win, false)
                 end, { buffer = 0 })
               end
             end,
