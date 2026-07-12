71bab7514a45992eb6b0b18000fb539aed8f48c5
Randy West <randywest55@gmail.com>
2026-07-12 14:07:03 -0700

    Switch display layout management to Display Configuration Switcher

--- install.sh
+++ install.sh
@@ -503,7 +503,9 @@ curl -fsSL https://antigravity.google/cli/install.sh | bash
 # install Copilot CLI
 curl -fsSL https://gh.io/copilot-install | bash
 
-# install gdctl dependencies along with my laptop monitor toggle script
+# install gdctl dependencies. I used to have a script (now archived) to toggle
+# display layout that used gdctl, but I switched to an extension that does the
+# same thing a little more nicely with menu integration and shortcuts. I'm
+# still keeping this around so I can use gdctl
 sudo apt-get install -y libcairo2-dev libxt-dev libgirepository-2.0-dev
 pip install pycairo PyGObject
-ln -sf $PATH_TO_DOT_FILES/laptop-monitor-toggle $HOME/.local/bin
