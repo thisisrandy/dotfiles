172d713ab264c005521f06b830ab5070fc860fdc
Randy West <randywest55@gmail.com>
2026-07-20 16:50:50 -0700

    Add note about lua version needed for installation

--- lazy-nvim-config/lua/plugins/neorg.lua
+++ lazy-nvim-config/lua/plugins/neorg.lua
@@ -5,6 +5,7 @@ return {
   config = true,
   -- These are implicit, but setting them explicitly seems to quiet a not found
   -- error that's probably a race condition
+  -- NOTE: These won't build without lua 5.1 and luarocks installed against it
   dependencies = {
     "nvim-neorg/tree-sitter-norg",
     "nvim-neorg/tree-sitter-norg-meta",
