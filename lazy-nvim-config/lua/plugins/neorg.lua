c4ef95e58c3dd143d34efb4751ef070c99cc9894
Randy West <randywest55@gmail.com>
2026-08-10 14:11:38 -0700

    Add + config neorg modules

--- lazy-nvim-config/lua/plugins/neorg.lua
+++ lazy-nvim-config/lua/plugins/neorg.lua
@@ -2,7 +2,20 @@ return {
   "nvim-neorg/neorg",
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
-  config = true,
+  opts = {
+    load = {
+      ["core.defaults"] = {},
+      ["core.dirman"] = {
+        config = {
+          workspaces = {
+            notes = "~/git/notes",
+          },
+          index = "index.norg",
+        },
+      },
+      ["core.concealer"] = {},
+    },
+  },
   -- These are implicit, but setting them explicitly seems to quiet a not found
   -- error that's probably a race condition
   -- NOTE: These won't build without lua 5.1 and luarocks installed against it
